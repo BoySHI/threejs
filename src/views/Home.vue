0eac016a1847ed8cd4cadcca6d1efc36dc75478d
BoySHI <1508577385@qq.com>
2022-05-10 15:05:51 +0800

    增加备注

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,14 @@
  * @Description: 
  * @Author: shiby
  * @Date: 2022-05-07 16:25:07
- * @LastEditTime: 2022-05-10 09:53:02
+ * @LastEditTime: 2022-05-10 15:04:55
  * @LastEditors: shiby
  * @Reference: 
 -->
 <template>
   <div class="scene">
     <button @click="increase">增加{{count}}</button>
+    <!-- 稍微修改下 -->
   </div>
 </template>
 
